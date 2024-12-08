8bf67617ee352c02054bb04bede3c1caec6e5b6b
Sais-opu <saidulislamapu2024@gmail.com>
2024-12-08 22:54:21 +0600

    today no more

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import banner from '../assets/banner.jpg';
+import BannerImg from '../assets/banner.jpg';
 const Home = () => {
     return (
         <div>
@@ -8,11 +8,22 @@ const Home = () => {
                 <p>Upgrade Your Tech Accessorize with Gadget Heaven Accessories</p>
                 <p>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
                 <Link to='/shop now'><button>shop now</button></Link>
-                <div className="absolute w-80 h-80 object-cover">
-                    <img src={banner} alt="" />
-                </div>
+                {/* <div className="absolute w-80 h-80 object-cover">
+                    <img src={BannerImg} alt="" />
+                </div> */}
             </div>
-            {/* card */}
+            {/* catagory */}
+            <p className='text-center'>Explore Cutting-Edge Gadgets</p>
+            <div className='flex'>
+                <div>
+                    {/* button */}
+                </div>
+                <div className='grid grid-cols-3'>
+                    {/* card */}
+
+                </div>
+                
+            </div>
         </div>
     );
 };
