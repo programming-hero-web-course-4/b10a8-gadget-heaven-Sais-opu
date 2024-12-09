862d48ecd7607696f1d127950e9f13a817aecc81
Sais-opu <saidulislamapu2024@gmail.com>
2024-12-09 11:42:20 +0600

    filter

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,23 @@
+
 import { useLoaderData, useParams } from "react-router-dom";
 import Data from "./Data";
+import { useEffect, useState } from "react";
 
 const Card = () => {
-    const data=useLoaderData()
-    const { category } = useParams()
-    console.log(data)
+    const data = useLoaderData();
+    const { category } = useParams();
+    const [cate, setCate] = useState([]);
+
+    useEffect(() => {
+        const filter = [...data].filter(item => item.category === category);
+        setCate(filter);
+    }, [category, data]);
+
     return (
         <div className="grid grid-cols-3">
-            {data.map(data =><Data key={data.product_id} data={data}></Data>)}
+            {cate.map((item) => (
+                <Data key={item.product_id} data={item} />
+            ))}
         </div>
     );
 };
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 
 const Data = ({data}) => {
     const {product_title}=data || {}
